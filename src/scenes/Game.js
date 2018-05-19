f3acb7df1f20c6c3be97fa7cb8eaed80f18c30eb
Sidaroth <sidaroth@protonmail.com>
2018-05-19 12:51:49 +0200

    Moved appropriate elements into the UI layer.

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -4,8 +4,6 @@ import config from '../config';
 import { createAudioManager } from '../components/AudioManager';
 import Box from '../entities/box';
 import UI from './UI';
-import Stats from 'stats-js';
-import * as dat from 'dat.gui';
 
 /**
  * Responsible for delegating the various levels, holding the various core systems and such.
@@ -27,20 +25,6 @@ export default class Game extends Phaser.Scene {
         this.UI = new UI();
         this.scene.add(config.SCENES.UI, this.UI, true);
         this.audioManager = createAudioManager(this.UI);
-        this.setupDatGui();
-    }
-
-    setupDatGui() {
-        this.gui = new dat.GUI();
-        this.gui.addFolder('Test folder');
-
-        this.guiData = {
-            name: 'name',
-        };
-        const guiController = this.gui.add(this.guiData, 'name');
-        guiController.onFinishChange((name) => {
-            console.log(name);
-        });
     }
 
     create() {
@@ -49,7 +33,7 @@ export default class Game extends Phaser.Scene {
         this.audioManager.playBgMusic();
         this.createCoin();
         this.addEntities();
-        this.setupPerformanceStats();
+        this.cameraSetup();
     }
 
     addEntities() {
@@ -61,33 +45,6 @@ export default class Game extends Phaser.Scene {
         console.log(this.entities);
     }
 
-    setupPerformanceStats() {
-        this.fpsStats = new Stats();
-        this.msStats = new Stats();
-        this.fpsStats.setMode(0);
-        this.msStats.setMode(1);
-
-        this.fpsStats.domElement.style.position = 'absolute';
-        this.fpsStats.domElement.style.left = '0px';
-        this.fpsStats.domElement.style.top = '0px';
-
-        this.msStats.domElement.style.position = 'absolute';
-        this.msStats.domElement.style.left = '80px';
-        this.msStats.domElement.style.top = '0px';
-
-        document.body.appendChild(this.fpsStats.domElement);
-        document.body.appendChild(this.msStats.domElement);
-
-        this.events.on('preupdate', () => {
-            this.fpsStats.begin();
-            this.msStats.begin();
-        });
-        this.events.on('postupdate', () => {
-            this.fpsStats.end();
-            this.msStats.end();
-        });
-    }
-
     update(time, delta) {}
 
     createCoin() {
@@ -95,7 +52,7 @@ export default class Game extends Phaser.Scene {
     }
 
     cameraSetup() {
-        this.cameras.main.startFollow(this.vehicles.get(0));
+        // this.cameras.main.startFollow(this.player); // or whatever else.
         this.cameras.main.setViewport(0, 0, config.GAME.VIEWWIDTH, config.GAME.VIEWHEIGHT);
         this.cameras.main.setZoom(0.8);
     }
--- src/scenes/UI.js
+++ src/scenes/UI.js
@@ -1,5 +1,7 @@
 import Phaser from 'phaser';
 import config from '../config';
+import Stats from 'stats-js';
+import * as dat from 'dat.gui';
 
 /**
  * Layer/Scene for UI elements.
@@ -9,7 +11,50 @@ export default class UI extends Phaser.Scene {
         super(config.SCENES.UI);
     }
 
-    create() {}
+    create() {
+        this.setupDatGui();
+        this.setupPerformanceStats();
+    }
+
+    setupPerformanceStats() {
+        this.fpsStats = new Stats();
+        this.msStats = new Stats();
+        this.fpsStats.setMode(0);
+        this.msStats.setMode(1);
+
+        this.fpsStats.domElement.style.position = 'absolute';
+        this.fpsStats.domElement.style.left = '0px';
+        this.fpsStats.domElement.style.top = '0px';
+
+        this.msStats.domElement.style.position = 'absolute';
+        this.msStats.domElement.style.left = '80px';
+        this.msStats.domElement.style.top = '0px';
+
+        document.body.appendChild(this.fpsStats.domElement);
+        document.body.appendChild(this.msStats.domElement);
+
+        this.events.on('preupdate', () => {
+            this.fpsStats.begin();
+            this.msStats.begin();
+        });
+        this.events.on('postupdate', () => {
+            this.fpsStats.end();
+            this.msStats.end();
+        });
+    }
+
+    setupDatGui() {
+        this.gui = new dat.GUI();
+        this.gui.addFolder('Test folder');
+
+        this.guiData = {
+            name: 'name',
+        };
+        const guiController = this.gui.add(this.guiData, 'name');
+        guiController.onFinishChange((name) => {
+            console.log(name);
+        });
+    }
 
     bringToTop() {
         this.scene.bringToTop();
