7894f934a39e5d3b2ebdb22b3caf40085ed1f5cc
Sidaroth <sidaroth@protonmail.com>
2019-03-06 21:01:11 +0100

    Updated createState comments

--- src/utils/createState.js
+++ src/utils/createState.js
@@ -4,6 +4,8 @@ import pipe from './pipe';
 const createState = function createStateFunc(className = 'MyClass', mainState = {}, states = {}, overrides = {}) {
     const stateList = [];
     const pipes = {};
+
+    // Loop over all states (components).
     Object.keys(states).forEach((stateKey) => {
         const state = states[stateKey];
         stateList.push({
@@ -11,6 +13,7 @@ const createState = function createStateFunc(className = 'MyClass', mainState = 
             name: stateKey,
         });
 
+        // Loop over all properties on the state object. If functions, store them away for now.
         Object.keys(state).forEach((propKey) => {
             if (typeof state[propKey] === 'function') {
                 if (!pipes[propKey]) {
@@ -21,6 +24,8 @@ const createState = function createStateFunc(className = 'MyClass', mainState = 
         });
     });
 
+    // Loop over all functions stored in pipes, check for multiple usages of the same name.
+    // Automatically set up a pipe() structure for these.
     Object.keys(pipes).forEach((propKey) => {
         if (pipes[propKey].length > 1) {
             pipes[propKey] = pipe(...pipes[propKey]);
@@ -30,14 +35,19 @@ const createState = function createStateFunc(className = 'MyClass', mainState = 
     });
 
     getFunctionUsage(stateList, className);
+
+    // Creates a piped init/constructor that runs each __constructor() function in the different states.
+    // This allows a created class/state to have a constructor that is ran at create time.
     const init = pipe(...stateList.map(s => s.state.__constructor).filter(c => c));
 
     Object.assign(mainState, ...stateList.map(s => s.state), pipes, overrides);
 
+    // Cleans up any constructor still on the mainstate.
     if (mainState.__constructor) {
         delete mainState.__constructor;
     }
 
+    // actually calls the piped init constructor from above.
     init();
 
     return mainState;
