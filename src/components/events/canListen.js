d8ac35a35009d3bff0123ff88e93c86ca4a2c1f3
Sidaroth <sidaroth@protonmail.com>
2019-03-07 21:31:24 +0100

    Return the listener reference in canListen. Expose a dropListener function.

--- src/components/events/canListen.js
+++ src/components/events/canListen.js
@@ -4,19 +4,32 @@ const canListen = function canListenFunc(state) {
     const listeners = [];
 
     function listenOn(emitState, event, fn, context) {
-        listeners.push(emitState.on(event, fn, context));
+        const listener = emitState.on(event, fn, context);
+        listeners.push(listener);
+        return listener;
     }
 
     function listenOnce(emitState, event, fn, context) {
-        listeners.push(emitState.once(event, fn, context));
+        const listener = emitState.once(event, fn, context);
+        listeners.push(listener);
+        return listener;
     }
 
     function listenGlobal(event, fn, context) {
-        listeners.push(store.messageBus.on(event, fn, context));
+        const listener = store.messageBus.on(event, fn, context);
+        listeners.push(listener);
+        return listener;
     }
 
     function listenOnceGlobal(event, fn, context) {
-        listeners.push(store.messageBus.once(event, fn, context));
+        const listener = store.messageBus.once(event, fn, context);
+        listeners.push(listener);
+        return listener;
+    }
+
+    function dropListener(listener) {
+        listeners.splice(listeners.findIndex(l => l === listener), 1);
+        listener.drop();
     }
 
     function destroy() {
@@ -28,6 +41,7 @@ const canListen = function canListenFunc(state) {
     return {
         // props
         // methods
+        dropListener,
         listenOn,
         listenOnce,
         listenGlobal,
